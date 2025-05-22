01805a70e9594f92567565370163cb784f14dcb9
RobbyR98 <robbyrunge@yahoo.com>
2025-05-22 18:01:43 +0200

    feat: implement custom cursor with hover effects and integrate into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 
@@ -8,6 +8,31 @@ import { LandingPageComponent } from "./landing-page/landing-page.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+  
+  ngAfterViewInit() {
+    this.initCustomCursor();
+  }
+  
+  initCustomCursor() {
+    const cursor = document.querySelector('.custom-cursor') as HTMLElement;
+    
+    if (cursor) {
+      document.addEventListener('mousemove', (e) => {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+      });
+      
+      document.querySelectorAll('a, button').forEach(item => {
+        item.addEventListener('mouseenter', () => {
+          cursor.style.transform = 'translate(-50%, -50%) scale(1.2)';
+        });
+        
+        item.addEventListener('mouseleave', () => {
+          cursor.style.transform = 'translate(-50%, -50%) scale(1)';
+        });
+      });
+    }
+  }
 }
