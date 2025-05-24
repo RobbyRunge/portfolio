b6d9bc0419c87e460b583266cab1663d3cd944a7
RobbyR98 <robbyrunge@yahoo.com>
2025-05-24 10:46:10 +0200

    feat: implement responsive mobile menu with toggle functionality and styling adjustments

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  mobileMenuOpen = false;
 
+  constructor(private elementRef: ElementRef) {}
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+    document.body.style.overflow = this.mobileMenuOpen ? 'hidden' : '';
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.mobileMenuOpen) return;
+
+    const mobileMenu = this.elementRef.nativeElement.querySelector('.mobile-menu');
+    const hamburgerIcon = this.elementRef.nativeElement.querySelector('.hamburger-icon');
+    
+    if (mobileMenu && !mobileMenu.contains(event.target) && 
+        hamburgerIcon && !hamburgerIcon.contains(event.target)) {
+      this.mobileMenuOpen = false;
+      document.body.style.overflow = '';
+    }
+  }
 }
