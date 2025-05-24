fa89bce175c49cb13338318a4e87a85ea194754d
RobbyR98 <robbyrunge@yahoo.com>
2025-05-24 16:13:50 +0200

    feat: update language button functionality and images for improved user experience

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -8,8 +8,9 @@ import { Component, ElementRef, HostListener } from '@angular/core';
 })
 export class HeaderComponent {
   mobileMenuOpen = false;
+  isGerman = true;
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef) { }
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
@@ -22,11 +23,35 @@ export class HeaderComponent {
 
     const mobileMenu = this.elementRef.nativeElement.querySelector('.mobile-menu');
     const hamburgerIcon = this.elementRef.nativeElement.querySelector('.hamburger-icon');
-    
-    if (mobileMenu && !mobileMenu.contains(event.target) && 
-        hamburgerIcon && !hamburgerIcon.contains(event.target)) {
+
+    if (mobileMenu && !mobileMenu.contains(event.target) &&
+      hamburgerIcon && !hamburgerIcon.contains(event.target)) {
       this.mobileMenuOpen = false;
       document.body.style.overflow = '';
     }
+  }
+
+  get languageButtonSrc(): string {
+    return this.isGerman
+      ? "/assets/imgs/header/german-button.png"
+      : "/assets/imgs/header/english-button.png";
+  }
+
+  get languageButtonHoverSrc(): string {
+    return this.isGerman
+      ? "/assets/imgs/header/german-button-hover.png"
+      : "/assets/imgs/header/english-button-hover.png";
+  }
+
+  get languageButtonAlt(): string {
+    return this.isGerman ? "German button" : "English button";
+  }
+
+  get languageButtonHoverAlt(): string {
+    return this.isGerman ? "German button hover" : "English button hover";
+  }
+
+  changeButton() {
+    this.isGerman = !this.isGerman;
   }
 }
