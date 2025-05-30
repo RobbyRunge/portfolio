949005cea94faa83a8b3deae18825b2118f8a5c1
RobbyR98 <robbyrunge@yahoo.com>
2025-05-30 19:55:29 +0200

    feat: add colleagues section with styling and tests for improved portfolio presentation

---
+++ src/app/colleagues/colleagues.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colleagues',
+  imports: [],
+  templateUrl: './colleagues.component.html',
+  styleUrl: './colleagues.component.scss'
+})
+export class ColleaguesComponent {
+
+}
