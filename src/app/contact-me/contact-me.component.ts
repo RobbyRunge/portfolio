2fadb21498891f189a7d48d9f59612ee11303dd4
RobbyR98 <robbyrunge@yahoo.com>
2025-06-01 19:56:41 +0200

    fix: enhance privacy checkbox handling and improve error message display

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent {
+export class ContactMeComponent implements OnInit {
 
   http = inject(HttpClient)
 
@@ -21,7 +21,8 @@ export class ContactMeComponent {
     privacy: false
   };
 
-  mailTest = false;
+  privacyTouched = false;
+  formSubmitted = false;
 
   post = {
     endPoint: 'https://robby-runge.de/sendMail.php',
@@ -34,12 +35,25 @@ export class ContactMeComponent {
     },
   };
 
+  ngOnInit() {
+    this.privacyTouched = false;
+    this.formSubmitted = false;
+  }
+
+  onPrivacyClick() {
+    this.privacyTouched = true;
+  }
+
   onSubmit(ngForm: NgForm) {
+    this.formSubmitted = true;
+
     if (ngForm.submitted && ngForm.form.valid) {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
             ngForm.resetForm();
+            this.privacyTouched = false;
+            this.formSubmitted = false;
           },
           error: (error) => {
             console.error(error);
@@ -47,6 +61,5 @@ export class ContactMeComponent {
           complete: () => console.info('send post complete'),
         });
     }
-    ngForm.resetForm();
   }
 }
