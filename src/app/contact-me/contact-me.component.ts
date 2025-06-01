34aa3734a610b82508cf4718c1b7fe3abcfb33c9
RobbyR98 <robbyrunge@yahoo.com>
2025-06-01 16:32:15 +0200

    feat: update contact form to use ngModel for two-way data binding and enhance error message styling

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-me',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+export class ContactMeComponent {
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    privacy: false
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
   }
 }
