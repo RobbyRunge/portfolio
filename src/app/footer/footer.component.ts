3480e71b144f0cf66b2c3314212378e5b94038d1
RobbyR98 <robbyrunge@yahoo.com>
2025-06-02 19:24:03 +0200

    feat: implement legal notice overlay with open/close functionality

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  showLegalNotice = false;
 
+  openLegalNotice(event: Event): void {
+    event.preventDefault();
+    this.showLegalNotice = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeLegalNotice(): void {
+    this.showLegalNotice = false;
+    document.body.style.overflow = '';
+  }
 }
