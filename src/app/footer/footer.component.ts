309dba8a60dce0217dd0b50274fde7551b048018
RobbyR98 <robbyrunge@yahoo.com>
2025-06-02 20:16:59 +0200

    feat: implement internationalization for footer content with translations for legal notice and developer information

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../shared/translation.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [TranslateModule]
 })
 export class FooterComponent {
   showLegalNotice = false;
+
+  constructor(private translationService: TranslationService) {}
 
   openLegalNotice(event: Event): void {
     event.preventDefault();
