d1e1e4f215b448bbc4497672209e0420fa3f47b1
RobbyR98 <robbyrunge@yahoo.com>
2025-06-04 20:37:55 +0200

    feat: add FooterComponent to ImpressumComponent and update styles

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../shared/translation.service';
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-impressum',
-  imports: [TranslateModule],
+  imports: [TranslateModule, FooterComponent],
   standalone: true,
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
