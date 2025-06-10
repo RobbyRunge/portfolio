f35e12f6afc2e1b63ade37b33aa40da76bef566e
RobbyR98 <robbyrunge@yahoo.com>
2025-06-10 21:28:55 +0200

    feat: add AOS library for animations and implement data-aos attributes in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +14,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
 
   constructor(private router: Router) { }
-
   ngOnInit() {
+    // Initialize AOS
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 50
+    });
+
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
@@ -24,6 +32,8 @@ export class AppComponent implements OnInit {
           behavior: 'instant'
         });
       }
+      // Refresh AOS after route change
+      AOS.refresh();
     })
   }
 }
