a9370bd80af351c512a0a1797851fbaf6868b60c
RobbyR98 <robbyrunge@yahoo.com>
2025-11-03 22:59:52 +0100

    feat: update skill icon paths to new directory structure

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -17,15 +17,15 @@ interface Skill {
 })
 export class SkillsComponent {
   skills: Skill[] = [
-    { name: 'HTML', iconPath: 'assets/imgs/skills/html.png', alt: 'html icon' },
-    { name: 'CSS', iconPath: 'assets/imgs/skills/css.png', alt: 'css icon' },
-    { name: 'JavaScript', iconPath: 'assets/imgs/skills/javascript.png', alt: 'javascript icon' },
-    { name: 'Material Design', iconPath: 'assets/imgs/skills/material-design.png', alt: 'material-design icon' },
-    { name: 'TypeScript', iconPath: 'assets/imgs/skills/typescript.png', alt: 'typescript icon' },
-    { name: 'Angular', iconPath: 'assets/imgs/skills/angular.png', alt: 'angular icon' },
-    { name: 'Firebase', iconPath: 'assets/imgs/skills/firebase.png', alt: 'firebase icon' },
-    { name: 'Git', iconPath: 'assets/imgs/skills/git.png', alt: 'git icon' },
-    { name: 'Rest-Api', iconPath: 'assets/imgs/skills/rest-api.png', alt: 'rest-api icon' },
-    { name: 'Scrum', iconPath: 'assets/imgs/skills/scrum.png', alt: 'scrum icon' },
+    { name: 'HTML', iconPath: 'assets/imgs/skills/frontend/html.png', alt: 'html icon' },
+    { name: 'CSS', iconPath: 'assets/imgs/skills/frontend/css.png', alt: 'css icon' },
+    { name: 'JavaScript', iconPath: 'assets/imgs/skills/frontend/javascript.png', alt: 'javascript icon' },
+    { name: 'Material Design', iconPath: 'assets/imgs/skills/frontend/material-design.png', alt: 'material-design icon' },
+    { name: 'TypeScript', iconPath: 'assets/imgs/skills/frontend/typescript.png', alt: 'typescript icon' },
+    { name: 'Angular', iconPath: 'assets/imgs/skills/frontend/angular.png', alt: 'angular icon' },
+    { name: 'Firebase', iconPath: 'assets/imgs/skills/frontend/firebase.png', alt: 'firebase icon' },
+    { name: 'Git', iconPath: 'assets/imgs/skills/tools/git.png', alt: 'git icon' },
+    { name: 'Rest-Api', iconPath: 'assets/imgs/skills/frontend/rest-api.png', alt: 'rest-api icon' },
+    { name: 'Scrum', iconPath: 'assets/imgs/skills/tools/scrum.png', alt: 'scrum icon' },
   ];
 }
