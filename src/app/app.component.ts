c35be15f9c07317a400df402bd6fe83cd3635a8b
RobbyR98 <robbyrunge@yahoo.com>
2026-01-14 20:06:29 +0100

    fix: update job title translations and implement dynamic title in index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 // import { CookieBannerComponent } from './shared/cookie-banner/cookie-banner.component';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -17,8 +19,17 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   constructor(
     private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) { } ngOnInit() {
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private titleService: Title,
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.setTranslatedTitle();
+    this.translate.onLangChange.subscribe(() => {
+      this.setTranslatedTitle();
+    });
+
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
@@ -34,7 +45,13 @@ export class AppComponent implements OnInit, AfterViewInit {
           AOS.refresh();
         }, 100);
       }
-    })
+    });
+  }
+
+  private setTranslatedTitle() {
+    this.translate.get('HERO.JOB_TITLE').subscribe((translated: string) => {
+      this.titleService.setTitle(`Robby Runge - ${translated}`);
+    });
   }
 
   ngAfterViewInit() {
